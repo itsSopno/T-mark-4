9b661614c6cd76eb9c6b1b4a2afdc335d6131ffd
unknown <>
2026-03-19 19:05:44 +0600

    save changes

--- Src/Routes/auth.route.ts
+++ Src/Routes/auth.route.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { registerUserController, LoginUserController, logoutUserController, getMeController, createKeyboardCollectionControler } from "../Controllers/auth.controler.js";
+import { registerUserController, LoginUserController, logoutUserController, getMeController, createKeyboardCollectionControler, getAllKeyboards, getSingleKeyboard } from "../Controllers/auth.controler.js";
 import { authUser } from "../middleware/auth.middleware.js";
 
 const authRouter: Router = Router();
-
 authRouter.post("/register", registerUserController)
 authRouter.post("/login", LoginUserController)
 authRouter.get("/logout", logoutUserController)
 authRouter.get("/getMe", authUser, getMeController)
 authRouter.post("/createKeyboard", createKeyboardCollectionControler)
+authRouter.get("/getallkeyboard", getAllKeyboards)
+authRouter.get("/getsinglekeyboard/:id", getSingleKeyboard)
 
 export default authRouter;
