a8408833ec171ca5566601838eb0466fb45e7ddd
unknown <>
2026-03-29 18:48:19 +0600

    save changes

--- Src/Routes/auth.route.ts
+++ Src/Routes/auth.route.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { registerUserController, LoginUserController, logoutUserController, getMeController, optionalController } from "../Controllers/auth.controller.js";
-import { authUser } from "../middleware/auth.middleware.js";
 const authRouter: Router = Router();
 authRouter.post("/register", registerUserController)
 authRouter.post("/login", LoginUserController)
 authRouter.get("/logout", logoutUserController)
-authRouter.get("/getMe", authUser, getMeController)
-authRouter.put("/optional", authUser, optionalController)
+authRouter.get("/getMe", getMeController)
+authRouter.put("/optional", optionalController)
 export default authRouter;
