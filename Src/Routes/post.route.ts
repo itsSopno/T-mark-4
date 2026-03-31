5cb3334bd27c2818aca7048012cb63fc76cc276a
unknown <>
2026-04-01 02:34:29 +0600

    save changes

--- Src/Routes/post.route.ts
+++ Src/Routes/post.route.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import { createPost, getAllPosts, deletePostController, getPostByUserEmail, toggleLike, addComment } from "../Controllers/post.controller.js";
-import { authUser } from "../middleware/auth.middleware.js";
+
 
 const Postrouter = Router();
 
-Postrouter.post("/create", authUser, createPost);
+Postrouter.post("/create", createPost);
 Postrouter.get("/get", getAllPosts);
-Postrouter.delete("/delete/:id", authUser, deletePostController);
+Postrouter.delete("/delete/:id", deletePostController);
 Postrouter.get("/get/:email", getPostByUserEmail);
 
 // New Routes
-Postrouter.patch("/:id/like", authUser, toggleLike);
-Postrouter.post("/:id/comment", authUser, addComment);
+Postrouter.patch("/:id/like", toggleLike);
+Postrouter.post("/:id/comment", addComment);
 
 export default Postrouter;
